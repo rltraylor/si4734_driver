f8693f15e2ad6166e1b8f3c6a2dd4449eb045de3
Roger Traylor <traylor@ece.orst.edu>
2014-12-30 11:18:53 -0800

    Added interrupt from STC functionality.
    Also added wait for TWI and some delays
    which need to be checked sometime.

--- si4734.h
+++ si4734.h
@@ -1,9 +1,10 @@
 //Si4734 Addresses on the I2C bus
-#define SI4734_ADDRESS 0x22                       //fixed by Silicon Labs 
+#define SI4734_ADDRESS                0x22   //fixed by Silicon Labs 
 
 //property definitions
 #define GPO_IEN                       0x0001
-#define GPO_IEN_STCIEN_SHFT           0 
+#define GPO_IEN_STCIEN                0x0001 
+#define GPO_IEN_CTSIEN                0x0080 
 #define AM_SOFT_MUTE_MAX_ATTENUATION  0x3302
 #define AM_PWR_LINE_NOISE_REJT_FILTER 0x0100
 #define AM_CHANNEL_FILTER             0x3102
@@ -15,6 +16,8 @@
 
 //command definitions
 #define FM_TUNE_FREQ    0x20
+#define FM_PWR_UP       0x01
+#define AM_PWR_UP       0x01
 #define AM_TUNE_FREQ    0x40
 #define PWR_DOWN        0x11
 #define SET_PROPERTY    0x12
@@ -27,6 +30,10 @@
 #define AM_TUNE_STATUS  0x42
 #define AM_RSQ_STATUS   0x43
 #define AM_RSQ_STATUS_IN_INTACK 0x01
+#define GET_REV         0x10 
+
+#define FALSE           0x00
+#define TRUE            0x01
 
 //si4734.c function prototypes
 uint8_t get_int_status();
@@ -42,4 +49,6 @@ void    am_pwr_up();
 void    sw_pwr_up();
 void    radio_pwr_dwn();
 void    set_property();
+void    get_rev();
+void    get_fm_rsq_status();
 
